bd743d68ea83f3f61208ef07c8a0ed436f5da839
tokorom <tokorom@gmail.com>
2021-09-02 16:05:22 +0900

    Detect screen captured

--- DetectScreenCaptured/ViewController.swift
+++ DetectScreenCaptured/ViewController.swift
@@ -5,10 +5,36 @@
 //  Created by Yuta Tokoro on 2021/09/02.
 //
 
+import Combine
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet private weak var sensitiveLabel: UILabel?
+
+    var cancellables: Set<AnyCancellable> = []
+
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        NotificationCenter.default.publisher(for: UIScreen.capturedDidChangeNotification)
+            .sink { notification in
+                self.detectCapturedDicChange(notification)
+            }
+            .store(in: &cancellables)
+    }
+
+    private func detectCapturedDicChange(_ notification: Notification) {
+        guard let screen = notification.object as? UIScreen else {
+            return
+        }
+
+        let sensitiveBackgroundColor: UIColor
+        if screen.isCaptured {
+            sensitiveBackgroundColor = .black
+        } else {
+            sensitiveBackgroundColor = .clear
+        }
+
+        sensitiveLabel?.backgroundColor = sensitiveBackgroundColor
     }
 }

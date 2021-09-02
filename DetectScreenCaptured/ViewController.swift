0f8803c61b970328097a722865e16814693ab375
tokorom <tokorom@gmail.com>
2021-09-02 15:53:31 +0900

    First commit

---
+++ DetectScreenCaptured/ViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ViewController.swift
+//  DetectScreenCaptured
+//
+//  Created by Yuta Tokoro on 2021/09/02.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
